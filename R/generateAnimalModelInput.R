dcbe4f52cc50ba70a299796d9697417766c8f2f9
Diogo Melo <diogro@gmail.com>
2020-10-09 21:47:34 -0300

    HDF5 output

--- R/generateAnimalModelInput.R
+++ R/generateAnimalModelInput.R
@@ -1,10 +1,13 @@
 #' Generate input for BSFG model in Matlab and Julia
 #'
-#' @param formula formula specifing fixed effects and response variables
-#' @param data data.frame with data, default is ratonesdf
-#' @param A relatinship matrix from pedigree
-#' @param out_folder folder to create the Matlab input files
+#' @param formula formula specifying fixed effects and response variables
+#' @param data data.frame with data. 
+#' @param A relationship matrix from pedigree
+#' @param IDcol Which column has the individual IDs which match the A matrix rownames.
+#' @param out_type Default is none to output object to R. Choose 'julia' for HDF5 or 'matlab' for mat
+#' @param out_folder folder to create the Matlab or Julia input files
 #' @importFrom R.matlab writeMat
+#' @importFrom rhdf5 h5createFile h5createGroup h5write H5close
 #' @importFrom stats lm as.formula model.matrix
 #' @return
 #' list with animal model input objects
@@ -17,20 +20,34 @@
 #'           paste(names(dplyr::select(ratonesdf, IS_PM:BA_OPI)),
 #'                 collapse = ", "), ") ~ SEX + AGE")
 #' animal_model_data = generateAnimalModelInput(formula, ratonesdf,
-#'                                              ratones_ped$A, out_foldel = "./")
-genAnimalModelInput <- function(formula, data, A,
-                                     IDcol = "ID", out_folder = NULL){
-
+#'                                              ratones_ped$A, out_folder = "./")
+genAnimalModelInput <- function(formula, data, A, IDcol = "ID", 
+                                out_type = c("none", "julia", "matlab"),
+                                out_folder = "BSFG_run"){
+  type = match.arg(type)
   model = lm(as.formula(formula), data = data)
   Y = model$model[[1]]
   X = model.matrix(model)
   pos = sapply(data[[IDcol]], function(x) which(x == rownames(A)))
   A = as.matrix(A[pos, pos])
   Z = diag(nrow(A))
-  if(!is.null(out_folder)){
+  if(out_type != "none"){
     if(!dir.exists(out_folder))
       dir.create(file.path(out_folder), showWarnings = FALSE)
-    writeMat(paste0(out_folder, "/setup.mat"), A = A, X = t(X), Y = Y, Z_1 = Z)
+    if(out_type == "matlab")
+      writeMat(paste0(out_folder, "/setup.mat"), A = A, X = t(X), Y = Y, Z_1 = Z)
+    else if(out_type=="julia"){
+      out_file = file.path(out_folder, "/setup.h5")
+      if(file.exists(out_file))
+        file.remove(out_file)
+      h5createFile(out_file)
+      h5createGroup(out_file, "Input")
+      h5write(Y, out_file, name="Input/Y")
+      h5write(t(X), out_file, name="Input/X")
+      h5write(as.matrix(A), out_file, name="Input/A")
+      h5write(Z, out_file, name="Input/Z_1")
+      H5close()
+    }
   }
   return(list(K = ncol(Y),
               J = ncol(X),
