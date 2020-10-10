45ae6101a4f9e3e4274fafd6b4b8ba6f6330f6e5
Diogo Melo <diogro@gmail.com>
2020-10-10 08:23:40 -0300

    writing HDF5 files

--- R/generateAnimalModelInput.R
+++ R/generateAnimalModelInput.R
@@ -24,7 +24,7 @@
 genAnimalModelInput <- function(formula, data, A, IDcol = "ID", 
                                 out_type = c("none", "julia", "matlab"),
                                 out_folder = "BSFG_run"){
-  type = match.arg(type)
+  out_type = match.arg(out_type)
   model = lm(as.formula(formula), data = data)
   Y = model$model[[1]]
   X = model.matrix(model)
@@ -40,13 +40,7 @@ genAnimalModelInput <- function(formula, data, A, IDcol = "ID",
       out_file = file.path(out_folder, "/setup.h5")
       if(file.exists(out_file))
         file.remove(out_file)
-      h5createFile(out_file)
-      h5createGroup(out_file, "Input")
-      h5write(Y, out_file, name="Input/Y")
-      h5write(t(X), out_file, name="Input/X")
-      h5write(as.matrix(A), out_file, name="Input/A")
-      h5write(Z, out_file, name="Input/Z_1")
-      H5close()
+      saveHDF5animalModel(Y, X, A, Z, out_file)
     }
   }
   return(list(K = ncol(Y),
@@ -56,4 +50,29 @@ genAnimalModelInput <- function(formula, data, A, IDcol = "ID",
               X = X,
               Y = Y,
               Z = Z))
+}
+
+#' Generate input for BSFG model in Julia
+#'
+#' @param Y matrix of observed phenotypes
+#' @param X fixed effects model matrix 
+#' @param A relationship matrix from pedigree. Use nadiv package.
+#' @param Z Which column has the individual IDs which match the A matrix rownames.
+#' @param out_file output filename.
+#' @importFrom rhdf5 h5createFile h5createGroup h5write H5close
+#' @return
+#' list with animal model input objects
+#' @export
+#' @examples
+#' library(ratones)
+#' saveHDF5animalModel(Y, X, A, Z, "setup.h5")
+saveHDF5animalModel = function(Y, X, A, Z, out_file){
+  h5createFile(out_file)
+  h5createGroup(out_file, "Input")
+  h5write(Y, out_file, name="Input/Y")
+  h5write(t(X), out_file, name="Input/X")
+  h5write(as.matrix(A), out_file, name="Input/A")
+  h5write(Z, out_file, name="Input/Z_1")
+  H5close()
+  return(list(Y, X, A, Z))
 }
